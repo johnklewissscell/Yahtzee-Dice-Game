52867234e408127bb9b07dee8b98691edca2d055
johnklewissscell <johnklewisss@gmail.com>
2026-03-06 15:17:39 -0500

    fixer
    Co-authored-by: Amir5470 <Amir5470@users.noreply.github.com>

---
+++ public/api.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB2c3iRRqzuGfReQ84sDQGwjrn2QumefNE",
+  authDomain: "yahtzee-onlinegame.firebaseapp.com",
+  databaseURL: "https://yahtzee-onlinegame-default-rtdb.firebaseio.com",
+  projectId: "yahtzee-onlinegame",
+  storageBucket: "yahtzee-onlinegame.firebasestorage.app",
+  messagingSenderId: "723861302840",
+  appId: "1:723861302840:web:f5603aac4b931f348d00cd",
+  measurementId: "G-9RSG0HRQZK",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+export { db };
